976f2a7bd0a8bb8b3db07b8f57d172e64f19ae52
Your Name <you@example.com>
2021-11-09 14:30:55 +0100

    Javascript események kezelése

---
+++ main.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", init);
+
+let kimenet;
+
+function init(){
+    kimenet = document.getElementById("kimenet");
+    document.getElementById("mintaszoveg").addEventListener("input", mintaszovegValtozas);
+    document.getElementById("betumeret").addEventListener("input", betumeretValtozas);
+    document.getElementById("szovegszin").addEventListener("input", szovegszinValtozas);
+    document.getElementById("hatterszin").addEventListener("input", hatterszinValtozas);
+    document.getElementById("alaphelyzet").addEventListener("click", alaphelyzet);
+}
+
+function mintaszovegValtozas(e){
+    kimenet.innerHTML = e.target.value;
+}
+
+function betumeretValtozas(e){
+    kimenet.style.fontSize = e.target.value + "pt";
+}
+
+function szovegszinValtozas(e){
+    kimenet.style.color = e.target.value;
+}
+
+function hatterszinValtozas(e){
+    kimenet.style.backgroundColor = e.target.value;
+}
+
+function alaphelyzet(){
+    document.getElementById("mintaszoveg").value = "Árvíztűrő tükörfúrógép";
+    document.getElementById("betumeret").value = 12;
+    document.getElementById("szovegszin").value = "#000000";
+    document.getElementById("hatterszin").value = "#FFFFFF";
+    kimenet.innerHTML = "Árvíztűrő tükörfúrógép";
+    kimenet.style.backgroundColor = "#FFFFFF";
+    kimenet.style.color = "#000000";
+    kimenet.style.fontSize = "12pt";
+}
